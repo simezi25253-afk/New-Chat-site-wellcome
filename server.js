4e33a5f249a9ecdedbafd4ae6fb88bcafe2b09b4
simezi25253-afk <simezi25253@gmail.com>
2026-03-18 08:53:30 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+require("dotenv").config();
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.use("/", authRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running"));
