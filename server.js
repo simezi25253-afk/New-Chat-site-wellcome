14613d2f6754f92391c875f14e92c5568831186c
simezi25253-afk <simezi25253@gmail.com>
2026-03-31 19:53:45 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,20 +4,26 @@ const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth");
 require("dotenv").config();
 
+// EJS設定
 app.set("view engine", "ejs");
-app.set("views", __dirname + "/views");
+
+// Render環境ではプロジェクトが1階層深くなるため、viewsのパスを修正
+app.set("views", __dirname + "/New-Chat-site-wellcome/views");
 
 app.use(express.urlencoded({ extended: true }));
 
+// MongoDB接続
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
+// ルート
 app.get("/", (req, res) => {
   res.render("index");
 });
 
 app.use("/", authRoutes);
 
+// サーバー起動
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server running"));
